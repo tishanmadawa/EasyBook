a9457368d3b56720573eb52854586fd7bf49917a
senalishalika <cs4shalika@gmail.com>
2017-04-08 07:46:46 +0530

    add admin main page

---
+++ EasyBook/HotelBook/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelBook.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Request()
+        {
+            return View();
+        }
+    }
+}
