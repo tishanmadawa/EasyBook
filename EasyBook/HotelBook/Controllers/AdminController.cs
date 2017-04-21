d82280636fa1cbf92a832f8d59e35a6c083e955b
senalishalika <cs4shalika@gmail.com>
2017-04-21 20:43:29 +0530

    view request done

--- EasyBook/HotelBook/Controllers/AdminController.cs
+++ EasyBook/HotelBook/Controllers/AdminController.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using HotelBook.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Diagnostics;
 namespace HotelBook.Controllers
 {
     public class AdminController : Controller
@@ -14,9 +15,30 @@ namespace HotelBook.Controllers
             return View();
         }
 
+       
         public ActionResult Request()
         {
-            return View();
+            HotelDBContext hotelDb = new HotelDBContext();
+            List<Customer> range = hotelDb.Viewcustomer();
+            return View(range);
         }
+        public void setAccept(string email)
+        {
+            HotelDBContext hotelDb = new HotelDBContext();
+            Debug.WriteLine(email);
+
+            string h = email;
+            hotelDb.set(h);
+            hotelDb.sendMail(h);
+        }
+        public void setReject(string email)
+        {
+            HotelDBContext hotelDb = new HotelDBContext();
+
+
+            string h = email;
+            hotelDb.setr(h);
+        }
+
     }
 }
